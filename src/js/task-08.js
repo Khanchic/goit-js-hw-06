b9af64cce995918ecc7d76294ce17fe58f852b5d
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2022-10-05 17:47:55 +0300

    done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const formRef = document.querySelector(".login-form");
 
+formRef.addEventListener("submit", onFormSubmit);
+
+const userData = {
+  email: "",
+  password: "",
+};
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const passWord = event.currentTarget.elements.password.value;
+  if (email === "" || passWord === "") {
+    alert("please be a human");
+  } else {
+    userData.email = email;
+    userData.password = passWord;
+    formRef.reset();
+  }
+}
+console.log(onFormSubmit);
