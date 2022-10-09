f5099bd429b1151673ed0dba4addcaea03c89032
Aleksandr Khachich <95611562+Khanchic@users.noreply.github.com>
2022-10-09 17:56:37 +0300

    finally

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,8 +3,6 @@ const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 console.log(" ");
 for (const category of categories) {
-  const cathead = category.querySelector("h2");
-  const listItems = category.querySelectorAll("li");
-  console.log(`Category: ${cathead.textContent}`);
-  console.log(`Elements:${listItems.length}`);
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements:${category.lastElementChild.children.length}`);
 }
